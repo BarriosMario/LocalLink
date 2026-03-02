bcc3e746f4174b0cee66344567bf653d2dc1a887
VianneyAlvarezz <ialvarez.26873@umsa.edu.mx>
2026-03-01 22:55:46 -0600

    HomePage.xaml.cs
    Se agregaron algunos ejemplos de como se vería la interfaz

--- HomePage.xaml.cs
+++ HomePage.xaml.cs
@@ -1,9 +1,18 @@
-namespace LocalLink.Vistas;
+using LocalLink.Models;
+namespace LocalLink;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
+    public HomePage()
+    {
+        InitializeComponent();
+
+        var productos = new List<Product>
+        {
+            new Product { Name = "Café", Price = "$102.00", ImageUrl = "dotnet_bot.png" },
+            new Product { Name = "Laptop", Price = "$11000.00", ImageUrl = "dotnet_bot.png" },
+            new Product { Name = "Bicicleta", Price = "$2000.00", ImageUrl = "dotnet_bot.png" },
+            new Product { Name = "Pantalón", Price = "$200.00", ImageUrl = "dotnet_bot.png" }
+        };
     }
 }
