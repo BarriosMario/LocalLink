0c6294401f84db2d5a20e7c6aba0cdb50b44c552
Mario Antonio Barrios Aquino <mbarrios.21289@umsa.edu.mx>
2026-05-20 19:10:20 -0600

    Delete UserData.cs

--- UserData.cs
+++
@@ -1,9 +0,0 @@
-namespace LocalLink.Models;
-
-public class UserData
-{
-    public string Email { get; set; }
-    public string Username { get; set; }
-    public string PasswordHash { get; set; }
-    public string ProfileImagePath { get; set; } = "default_avatar.png";
-}
